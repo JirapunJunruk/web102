41723204bf319200a32513abb911fd055fb62790
unknown <jirapun0907@email.com>
2022-08-31 11:23:36 +0700

    first commit

---
+++ post.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+    <h1><center>Webboard DooDee</center></h1>
+<hr>
+ <center>ต้องการดูกระทู้หมายเลข 3
+    <br>
+    <br>
+ <table style = "border: 2px solid black; width: 50px">
+            <tr><td style="background-color :#6CD2FE;" colspan="2">แสดงความคิดเห็น</td></tr>
+            <tr><td><textarea nsme = "message" rows="3" cols="30"></textarea><td></tr>
+            <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+    </table>
+     </center>
+    <br>
+          <center>  <a href="index.html">กลับไปหน้าหลัก</a></center>
+</body>
+</html>
